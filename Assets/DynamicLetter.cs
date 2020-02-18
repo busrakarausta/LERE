75ae43735bd95218166dc1659e7338928351d72b
busrakarausta <karaustabusraa@gmail.com>
2020-02-18 17:57:31 +0300

    TestController is added for A

---
+++ Assets/DynamicLetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicLetter : MonoBehaviour
+{
+
+    public string letter;
+    Text letterText;
+
+    void Start()
+    {
+        letterText = GetComponent<Text>();
+        letterText.text = letter;
+    }
+}
---
+++ Assets/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestController : MonoBehaviour
+{
+    public string testObject;
+
+    public void TestClickedObject(string objChar, GameObject obj)
+    {
+        if (testObject == objChar)
+        {
+            Debug.Log("congrats");
+        }
+        else
+        {
+            Debug.Log("wrong");
+            obj.GetComponent<Color.>
+        }
+}
