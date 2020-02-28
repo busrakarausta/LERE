abe8737a71ab7f8490548ef7f5d5236d4bf27df5
elifsoolmaz <solmaz.elif96@gmail.com>
2020-02-28 18:29:07 +0300

    OrangeGame added.

---
+++ Assets/Scripts/Games/O-OrangeGame/OrangeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangeManager : MonoBehaviour
+{
+    private Vector3 currentOrangePos;
+    public Transform basketPos;
+    [HideInInspector]
+    public bool isOrangePosChanged=false;
+
+    private void Start()
+    {
+        currentOrangePos = basketPos.position;
+    }
+    public Vector3 GetCurrentApplePos()
+    {
+        if (isOrangePosChanged)
+        {
+            isOrangePosChanged = false;
+            return currentOrangePos;
+        }
+        else
+            return basketPos.position;
+    }
+    public void SetCurrentApplePos(Vector3 pos)
+    {
+        currentOrangePos = pos;
+        isOrangePosChanged = true;
+    }
+}
