bd1152d996f266695f0ea50d7798e0dae78a3165
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-03 14:38:37 +0300

    Letter Education removed from main scene

--- Assets/Scripts/LetterEducation/TracingPoint.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-//Tracing Point Script
-public class TracingPoint : MonoBehaviour
-{
-    public int index;//the index if the tracing point
-    public bool single_touch;//tracing point is a single touch or not
-}
