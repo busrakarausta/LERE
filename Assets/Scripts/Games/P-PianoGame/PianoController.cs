9b94e11335fdbdfffc3c07f675c449f4ad367122
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-05 13:57:09 +0300

    P - Piano game added.

--- Assets/Scripts/Games/P-PianoGame/PianoController.cs
+++ Assets/Scripts/Games/P-PianoGame/PianoController.cs
@@ -1,46 +1,54 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.Experimental.UIElements;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PianoController : MonoBehaviour, IPointerDownHandler
 {
     public GameObject canvas;
+    public Transform nota;
+
     public GameObject notaObject;
     private GameObject insObj;
-    public Transform nota;
+    private AudioSource source;
+    private Animator anim;
+    private int score=0;
     
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+   
     public void OnPointerDown(PointerEventData eventData)
     {
-        createSound(gameObject);
+        createSoundAndAnim(gameObject);
     }
 
-    public void CreateNota()
+    public IEnumerator CreateNota()
     {
-        notaObject.SetActive(true);
         insObj=Instantiate(notaObject, nota.position, Quaternion.identity);
+        insObj.SetActive(true);
         insObj.transform.SetParent(nota,true);
         insObj.transform.parent = canvas.transform;
-
         insObj.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 50f);
+        
+        yield return new WaitForSeconds(1f);
+        Destroy(insObj);
     }
 
-    public void createSound(GameObject gObject)
-    {
-        if (gObject.name == "Key_1")
-        {
-            Debug.Log("sound1");
-            CreateNota();
-        }
-        else if(gObject.name == "Key_2")
-        {
-            Debug.Log("sound2");
-            CreateNota();
-        }
+    public void createSoundAndAnim(GameObject gObject)
+    {      
+        anim.SetTrigger("Play");
+        source.Play();
+        StartCoroutine(CreateNota());
+        score++;
     }
-    
 }
 
 
