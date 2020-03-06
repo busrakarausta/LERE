a029fc9b1530a8d8181e1e5af02d3f2cb40a14df
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-06 10:45:46 +0300

    I-Icecream game created, will be edit.

---
+++ Assets/Scripts/Games/I-IceCreamGame/IceCreamManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IceCreamManager : MonoBehaviour,IDragHandler
+{
+    public Transform cone;
+    private bool flag = false;
+    private float up=10;
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("draggggiinnggg..");
+        gameObject.transform.position = Input.mousePosition;
+        if (transform.position.x >= cone.position.x && flag)
+        {
+            TransformIcecream();
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.name == "Cone")
+        {
+            Debug.Log( gameObject.name +" perceive..");
+            flag = true;
+        }
+    }
+
+    private void TransformIcecream()
+    {
+        up += 10;
+        Debug.Log(gameObject.name + " accepted..");
+        transform.position= new Vector3(cone.transform.position.x, cone.transform.position.y+up, cone.transform.position.z);
+    }
+}
