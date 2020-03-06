d62cdb4aa36538b8f2ea927de5047130e54abce2
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-06 16:09:03 +0300

    K-Kangaroo game added.

---
+++ Assets/Scripts/Games/K-KangarooGame/KangarooController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KangarooController : MonoBehaviour,IPointerDownHandler
+{
+    private Animator anim;
+    private Animator anim_momChild;
+    public GameObject Mom_cangaroo;
+    public GameObject MomWithChild;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim_momChild = MomWithChild.GetComponent<Animator>();
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("clicked");
+        anim.SetTrigger("KangarooJump");
+        StartCoroutine(GetChild());
+    }
+
+    public IEnumerator GetChild()
+    { 
+        yield return new WaitForSeconds(0.5f);
+        Mom_cangaroo.SetActive(false);
+        gameObject.SetActive(false);
+        MomWithChild.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        anim_momChild.SetTrigger("MomKangaroo");
+    }
+}
