57256cd593ed7ebd5f74a70c5cb199204b037a0c
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-06 18:41:32 +0300

    Flower game edited.

---
+++ Assets/Scripts/Games/F-FlowerGame/PoteManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PoteManager : MonoBehaviour,IDragHandler
+{
+    public Transform basket;
+    private bool flag = false;
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("dragging..");
+        gameObject.transform.position = Input.mousePosition;
+        if (transform.position.x >= basket.position.x && flag)
+        {
+            Debug.Log( gameObject.name +" accepted..");
+            transform.position = basket.transform.position;
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.name == "Basket")
+        {
+            Debug.Log( gameObject.name +" perceive..");
+            flag = true;
+        }
+    }
+}
