bb0e95431ec789d893fa8d5979f0fa1b1af62bcc
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-06 18:24:34 +0300

    F-Flower game asset created, will be edit.

--- Assets/Scripts/Games/Q-QueenGame/CrownController.cs
+++ Assets/Scripts/Games/Q-QueenGame/CrownController.cs
@@ -12,16 +12,12 @@ public class CrownController : MonoBehaviour,IDragHandler
     void Awake()
     {
         source = GetComponent<AudioSource>();
-
     }
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("dragging..");
         gameObject.transform.position = Input.mousePosition;
         if (transform.position.x >= queen.position.x && flag)
         {
-            Debug.Log( gameObject.name +" accepted..");
-            //transform.position = queen.transform.position;
             transform.position= new Vector3(queen.transform.position.x-8f, queen.transform.position.y+140f, queen.transform.position.z);
             source.Play();
         }
@@ -31,7 +27,6 @@ public class CrownController : MonoBehaviour,IDragHandler
     {
         if (other.gameObject.name == "Girl")
         {
-            Debug.Log( gameObject.name +" perceive..");
             flag = true;
         }
     }
