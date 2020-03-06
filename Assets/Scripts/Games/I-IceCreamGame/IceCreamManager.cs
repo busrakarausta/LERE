256c041615ce96a421f27d71a8c561587bbd1dff
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-06 11:27:50 +0300

    I-Icecream game added.

--- Assets/Scripts/Games/I-IceCreamGame/IceCreamManager.cs
+++ Assets/Scripts/Games/I-IceCreamGame/IceCreamManager.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class IceCreamManager : MonoBehaviour,IDragHandler
 {
     public Transform cone;
     private bool flag = false;
-    private float up=10;
+    private float x_pos=-5f;
     
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("draggggiinnggg..");
         gameObject.transform.position = Input.mousePosition;
-        if (transform.position.x >= cone.position.x && flag)
+        if (transform.position.x >= cone.position.x && transform.position.y >= cone.position.y && flag)
         {
             TransformIcecream();
         }
@@ -23,15 +24,27 @@ public class IceCreamManager : MonoBehaviour,IDragHandler
     {
         if (other.gameObject.name == "Cone")
         {
-            Debug.Log( gameObject.name +" perceive..");
             flag = true;
         }
     }
 
     private void TransformIcecream()
     {
-        up += 10;
-        Debug.Log(gameObject.name + " accepted..");
-        transform.position= new Vector3(cone.transform.position.x, cone.transform.position.y+up, cone.transform.position.z);
+        if (gameObject.name == "Icecream1")
+        {
+            transform.position= new Vector3(cone.transform.position.x+x_pos, cone.transform.position.y+70f, cone.transform.position.z);
+        }
+        else if (gameObject.name == "Icecream2")
+        {
+            transform.position= new Vector3(cone.transform.position.x+x_pos, cone.transform.position.y+110f, cone.transform.position.z);
+        }
+        else if (gameObject.name == "Icecream3")
+        {
+            transform.position= new Vector3(cone.transform.position.x+x_pos, cone.transform.position.y+150f, cone.transform.position.z);
+        }
+        else if (gameObject.name == "Icecream4")
+        {
+            transform.position= new Vector3(cone.transform.position.x+x_pos, cone.transform.position.y+190f, cone.transform.position.z);
+        }
     }
 }
