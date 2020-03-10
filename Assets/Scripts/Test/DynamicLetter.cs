839872713f20315f5a5618f42ab1504f5a69f967
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-10 16:08:57 +0300

    Every scenes merged successfully.

--- Assets/Scripts/Test/DynamicLetter.cs
+++ Assets/Scripts/Test/DynamicLetter.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class DynamicLetter : MonoBehaviour
 {
-    public char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X','Y', 'Z'};
+   // public char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X','Y', 'Z'};
     public string letter;
     Text letterText;
 
