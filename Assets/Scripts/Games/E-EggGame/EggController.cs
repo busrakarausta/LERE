4da28b2726f5334cad7fc7a53fc30b573b863202
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-10 15:54:51 +0300

    Egg game created, will be edit.

---
+++ Assets/Scripts/Games/E-EggGame/EggController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EggController : MonoBehaviour, IPointerDownHandler
+{
+    private float step;
+    private float speed=5;
+    public GameObject panEmpty;
+    public GameObject crushedEgg;
+    public GameObject panEgg;
+    private bool arrived = false;
+
+
+
+    void Awake()
+    {
+       // source = cruchedEgg.GetComponent<AudioSource>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+      CruchEgg();
+    }
+
+    public void CruchEgg()
+        {
+        gameObject.SetActive(false);
+        crushedEgg.SetActive(true);
+
+        step = speed * Time.deltaTime;
+
+        while (!arrived)
+        {
+            Debug.Log("testtt");
+            
+            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            arrived = true;
+
+        }
+
+        crushedEgg.SetActive(false);
+        panEmpty.SetActive(false);
+        panEgg.SetActive(true);
+
+
+
+    }
+}
