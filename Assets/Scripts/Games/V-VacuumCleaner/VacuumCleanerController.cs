950fa3023707b554bd36325768c8e4032230a39c
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-11 15:52:04 +0300

    VacuumCleaner game added. Game prefab created.

---
+++ Assets/Scripts/Games/V-VacuumCleaner/VacuumCleanerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VacuumCleanerController : MonoBehaviour,IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        gameObject.transform.position = Input.mousePosition;
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("test");
+        if (other.gameObject.name == "Dirt")
+        {
+            other.gameObject.transform.position= new Vector3(other.transform.position.x, other.transform.position.y+20f, other.gameObject.transform.position.z);
+            yield return new WaitForSeconds(0.2f);
+            other.gameObject.SetActive(false);
+        }
+    }
+}
