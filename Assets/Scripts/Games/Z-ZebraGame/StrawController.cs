7979b934531b437d1abbd87cac6a14acb41e8f00
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-11 18:53:12 +0300

    Zebra game added.

---
+++ Assets/Scripts/Games/Z-ZebraGame/StrawController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StrawController : MonoBehaviour, IDragHandler
+{
+    public GameObject zebra1, zebra2, zebraAfter1, zebraAfter2;
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        gameObject.transform.position = Input.mousePosition;
+    }
+    
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OKEY");
+        if (other.gameObject.name == "Zebra1")
+        {
+            Debug.Log("aacepted.");
+            yield return new WaitForSeconds(0.2f);
+            zebra1.SetActive(false);
+            zebraAfter1.SetActive(true);
+        }
+       else if (other.gameObject.name == "Zebra2")
+        {
+            Debug.Log("aacepted.");
+            yield return new WaitForSeconds(0.2f);
+            zebra2.SetActive(false);
+            zebraAfter2.SetActive(true);
+        }
+        gameObject.transform.localPosition= new Vector3(0, 450,0);
+
+    }
+    
+}
