c5e49f439b651570b2a4f02f9af80363af7cc3c5
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-11 14:40:25 +0300

    Umbrella game added.

---
+++ Assets/Scripts/Games/U-UmbrellaGame/UmbrellaController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UmbrellaController : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject openedUmbrella;
+    public GameObject closedUmbrella;
+    private AudioSource source;
+    private int clickCount = 0;
+    
+    void Awake()
+    {
+        source = openedUmbrella.GetComponent<AudioSource>();
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(clickCount);
+        if (clickCount <= 2)
+        {
+            OpenUmbrella();
+        }
+        else
+        {
+            Debug.Log("pass game.");
+        }
+    }
+
+    private void OpenUmbrella()
+    {
+        clickCount++;
+        closedUmbrella.SetActive(false);
+        openedUmbrella.SetActive(true);
+        source.Play();
+    }
+}
