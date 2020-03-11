2d442e1a8c34400fde4dc85f6d2cacf9c9996533
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-11 14:57:14 +0300

    Flower game edited and completed.

--- Assets/Scripts/Games/F-FlowerGame/PoteManager.cs
+++ Assets/Scripts/Games/F-FlowerGame/PoteManager.cs
@@ -4,33 +4,32 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PoteManager : MonoBehaviour,IPointerDownHandler{
-
-    private Vector3 targetPosition;
-    private Vector3 currentPosition;
-    public Transform petal;
-
-    public void OnPointerDown(PointerEventData eventData)
+    
+    private Rigidbody2D petal;
+    
+    void Awake()
     {
-       //StartCoroutine(MovePetal());
+        petal = gameObject.GetComponent<Rigidbody2D>();
     }
     
-    /*
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       StartCoroutine(MovePetal());
+    }
+
     public IEnumerator MovePetal()
     {
-       
-        currentPosition= gameObject.transform.position;
-        if(gameObject.name== "Petal1" || gameObject.name== "Petal2" || gameObject.name== "Petal4")
+        if (gameObject.name == "Petal1" || gameObject.name == "Petal3" || gameObject.name == "Petal5")
         {
-        targetPosition= new Vector3(gameObject.transform.position.x, gameObject.transform.position.y-10f, gameObject.transform.position.z);
-        gameObject.transform.position= Vector3.Lerp(transform.position, transform.position+targetPosition , 50f / 10f * Time.deltaTime);
+            petal.velocity = new Vector2(-120f, -200f);
+            yield return new WaitForSeconds(5);
+            gameObject.SetActive(false);
         }
         else
         {
-            targetPosition= new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+10f, gameObject.transform.position.z);
-            gameObject.transform.position= Vector3.Lerp(transform.position, transform.position+targetPosition , 50f / 10f * Time.deltaTime);
+            petal.velocity = new Vector2(120f, -200f);
+            yield return new WaitForSeconds(5);
+            gameObject.SetActive(false);
         }
-        yield return new WaitForSeconds (1);
-        
     }
-    */
 }
