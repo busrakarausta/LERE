9e874a4888744da0bb8b303b1950093120e86bfa
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-11 18:03:55 +0300

    Watermelon game added.

---
+++ Assets/Scripts/Games/W-WatermelonGame/WatermelonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WatermelonManager : MonoBehaviour
+{
+    public GameObject knife;
+    public GameObject watermelon;
+    public GameObject watermelonCut;
+    
+   
+
+    void Update()
+    {
+        gameObject.transform.position = Input.mousePosition;
+    }
+    
+
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("test");
+        if (other.gameObject.name == "Watermelon")
+        {
+            yield return new WaitForSeconds(1.5f);
+            other.gameObject.SetActive(false);
+            watermelonCut.SetActive(true);
+            
+            knife.gameObject.SetActive(false);
+        }
+    }
+}
