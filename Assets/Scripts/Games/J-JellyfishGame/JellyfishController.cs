cb1fe6a90910cbc5602f10ffd1239d5f65d30858
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-12 18:01:57 +0300

    J - Jellyfish game added.

---
+++ Assets/Scripts/Games/J-JellyfishGame/JellyfishController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JellyfishController : MonoBehaviour,IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        gameObject.transform.position = Input.mousePosition;
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.name == "Cave")
+        {
+            yield return new WaitForSeconds(0.5f);
+           gameObject.SetActive(false);
+        }
+    }
+}
