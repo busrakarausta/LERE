e56766f7b6ce0566fc0cfda8b0b2b170af322625
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-12 14:11:23 +0300

    Hamburger game added.

---
+++ Assets/Scripts/Games/H-HamburgerGame/HamburgerCreator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HamburgerCreator : MonoBehaviour, IPointerDownHandler
+{
+    public Transform bottom;
+    public float move=50f;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        AddToBurger();
+    }
+
+    private void AddToBurger()
+    {
+        if (gameObject.name == "Meat")
+        {
+            gameObject.transform.position = new Vector3(bottom.position.x, bottom.position.y + move, bottom.position.z);
+        }
+        else if (gameObject.name == "Lettuce")
+        {
+            gameObject.transform.position=  new Vector3(bottom.position.x, bottom.position.y + move+70f, bottom.position.z);
+        }
+        else if (gameObject.name == "Top")
+        {
+            gameObject.transform.position=  new Vector3(bottom.position.x, bottom.position.y + move+150f, bottom.position.z);
+        }
+    }
+    
+}
