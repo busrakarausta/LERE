28eacabb2c165287c9c6379f3eac98a6ccd8dec7
elifsoolmaz <solmaz.elif96@gmail.com>
2020-03-12 15:27:38 +0300

    Yogurt game added.

---
+++ Assets/Scripts/Games/Y-YogurtGame/YogurtManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class YogurtManager : MonoBehaviour, IDragHandler
+{
+    public GameObject yogurt;
+    public GameObject yogurtWithOnion;
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = yogurtWithOnion.GetComponent<AudioSource>();
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        gameObject.transform.position = Input.mousePosition;
+    }
+    
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("test");
+        if (other.gameObject.name == "Yogurt")
+        {
+            yield return new WaitForSeconds(0.7f);
+            yogurt.SetActive(false);
+            yogurtWithOnion.SetActive(true);
+            source.Play();
+        }
+        gameObject.SetActive(false);
+    }
+
+}
