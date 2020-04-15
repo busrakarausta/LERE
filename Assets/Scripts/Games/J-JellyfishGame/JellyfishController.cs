6b3abb870bbf2e2650d527d286dc6f9201d70b33
busrakarausta <33478107+busrakarausta@users.noreply.github.com>
2020-04-15 17:47:06 +0300

    games and educations are successfully merged

--- Assets/Scripts/Games/J-JellyfishGame/JellyfishController.cs
+++ Assets/Scripts/Games/J-JellyfishGame/JellyfishController.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 
 public class JellyfishController : MonoBehaviour,IDragHandler
 {
+
     public void OnDrag(PointerEventData eventData)
     {
         gameObject.transform.position = Input.mousePosition;
@@ -14,7 +15,16 @@ public class JellyfishController : MonoBehaviour,IDragHandler
     {
         if (other.gameObject.name == "Cave")
         {
-            yield return new WaitForSeconds(0.5f);
+            Vector3 lastScale = transform.localScale;
+            Vector3 finalScale = lastScale * 0.0001f;
+          
+           while(transform.localScale.magnitude >= finalScale.magnitude)
+           {
+                transform.localScale = transform.localScale * 0.9f;
+                yield return new WaitForSeconds(0.1f);
+            }
+           // source.Play();
+            yield return new WaitForSeconds(2f);
            gameObject.SetActive(false);
         }
     }
