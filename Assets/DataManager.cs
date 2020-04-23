9b8b8d5cc73998b9b31a6b4f431f5a2142330bde
busrakarausta <33478107+busrakarausta@users.noreply.github.com>
2020-04-23 19:34:22 +0300

    Parent Screen set up and Data Manager initialized

---
+++ Assets/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+
+    [Header("Data Keys")]
+
+    [SerializeField]
+    private string activeGameKey = "ActiveGameCount";
+
+    private int _activeGameCount;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        instance = this;
+    }
+
+    public void SetActiveGameCount(int value=1)
+    {
+        _activeGameCount = value;
+
+        PlayerPrefs.SetInt(activeGameKey, _activeGameCount);
+    }
+
+}
---
+++ Assets/ParentUIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParentUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Slider gameCountSlider;
+    [SerializeField]
+    private Text sliderShownValue;
+
+    private int gameCountValue;
+
+
+    private void Awake()
+    {
+        gameCountValue = (int)gameCountSlider.value;
+        sliderShownValue.text = gameCountValue.ToString();
+        DataManager.instance.SetActiveGameCount(gameCountValue);
+    }
+
+    public void ChangeGameCount()
+    {
+        gameCountValue = (int)gameCountSlider.value;
+        sliderShownValue.text = gameCountValue.ToString();
+
+        DataManager.instance.SetActiveGameCount(gameCountValue);
+    }
+
+    public void BackToHome()
+    {
+        ApplicationManager.instance.LoadHomeScene();
+    }
+}
