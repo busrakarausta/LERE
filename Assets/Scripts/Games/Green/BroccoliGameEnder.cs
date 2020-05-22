ccbed4f26927eef4b6d95e5f355add2e26eb8016
elifsoolmaz <solmaz.elif96@gmail.com>
2020-05-22 16:18:42 +0300

    Color green's education and game prefabs added.

---
+++ Assets/Scripts/Games/Green/BroccoliGameEnder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BroccoliGameEnder : MonoBehaviour
+{
+    private int broccoliCount = 0;
+    private int totalCount = 3;
+
+    public void IncreaseCount()
+    {
+        broccoliCount++;
+        if (broccoliCount == totalCount)
+        {
+            LevelController.instance.OnLevelEnd();
+        }
+    }
+}
